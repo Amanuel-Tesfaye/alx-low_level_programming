c9d880e2155f4668b94ed5a5c9f5193203e6d00c
Amanuel-Tesfaye <amanueltesfaye213@gmail.com>
2023-06-21 11:06:28 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,8 @@
-#include "holberton.h"
-
+#include <stdio.h>
 /**
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
-*/
-
-int main(void)
+ * put the character C to stdio
+ */
+int _putchar(char c)
 {
-	int i;
-	char p[] = "Holberton";
-
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
